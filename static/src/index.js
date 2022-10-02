768bb9281c315f0adbacac4eda7117116ad461d5
George Morais <george.morais@sensedata.com.br>
2022-10-02 20:08:14 -0300

    começando frontend

--- static/src/index.js
+++ static/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App';
+import SignUpPage from './components/signup_page/signup_page';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <SignUpPage/>
 );
 
